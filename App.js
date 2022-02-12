b9aa2dfdf2af20b6a83f9d6a678a243d122b6b66
Jake Garcia <j.garcia@idreamers.com>
2022-02-12 14:53:31 -0800

    Provider and PersistGate have been added and they have been supplied the store as well as a persistor respectively.

--- App.js
+++ App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { StyleSheet, } from 'react-native';
 import MainStack from './routes/MainStack';
 
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import { store, persistor } from './redux';
+
+
 export default function App() {
   return (
-    <MainStack />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MainStack />
+      </PersistGate>
+    </Provider>
   );
 }
 
